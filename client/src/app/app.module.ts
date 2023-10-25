9634d7957955aa541d50b48d303f904372969131
Nilay Oyat <nilayoyat@gmail.com>
2023-10-25 14:05:09 +0100

    End of Section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopService } from './shop/shop.service';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
